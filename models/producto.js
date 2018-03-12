d115817568e95a8b8f05bd0e8808cffcd7bafdf4
gabriel <gabriel@agetic.agetic>
2018-03-12 12:46:09 -0400

    agregando registro y listado de productos

--- models/producto.js
+++ models/producto.js
@@ -3,12 +3,13 @@ var mongoose = require('mongoose');
 var Esquema = mongoose.Schema;
 var productoEsquema = ({
     nombre : String,
-    detalle : String,
-    precioBOB : String,
-    categoria :  String,
+    descripcion : String,
     estado : String,
+    precioBOB : String,
+    precioUSD : String,
     imagen : String,
     usuario : String,
+    fecha : String
 });
 
 module.exports=mongoose.model('Producto', productoEsquema);
--- routes/productoRoute.js
+++ routes/productoRoute.js
@@ -5,5 +5,7 @@ var md_autenticar=require('../middlewares/autenticar');
  var api = express.Router();
 
  api.get('/homeProducto', md_autenticar.authSeguro,productoController.productoHome);
-
+ api.post('/producto',md_autenticar.authSeguro,productoController.guardarProducto);
+ api.get('/producto/:id',md_autenticar.authSeguro,productoController.getProducto);
+ api.get('/productos/:pagina/:items?',md_autenticar.authSeguro,productoController.listarProductos);
  module.exports = api;
